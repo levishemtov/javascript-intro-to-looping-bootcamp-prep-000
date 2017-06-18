b917c0d9d450c5410918288f3dc30bbbdc9f532b
Levi Shemtov <leviuruguay@gmail.com>
2017-06-18 18:49:12 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,25 @@
+function forLoop(array){
+for (var i = 0; i < 25; i++) {
+  array.push( `"I am ${i} strange loop${i === 0 ? '' : 's'}."` );
+  }
+  return array
+}
+
+function whileLoop(n){
+  let countdown = n;
+  while (countdown > 0) {
+    console.log(--countdown)
+  }
+  return ('done')
+}
+
+function doWhileLoop(array){
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+
+  do {
+	array.pop()
+} while (maybeTrue() && array.length > 0);
+  return array
+}
